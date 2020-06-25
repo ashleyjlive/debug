b40a824b40ae1d628e79e27dc44336c461bf8c4c
ASHLEYDESKTOP\Ashley <ashley@parallaxnetworks.com>
2020-06-25 17:38:26 +1000

    Logging -- log.hrl: • Supplied additional data such as function name, module and the app of the caller. log@debug: • Removed expensive stacktrace and registered_name lookups and exported additional args.

--- include/log.hrl
+++ include/log.hrl
@@ -1,31 +1,58 @@
--define(debug(Msg), log@debug:do_log_event(debug, Msg)).
--define(debug(Tag, MsgOrDta), log@debug:do_log_event(debug, Tag, MsgOrDta)).
--define(debug(Tag, Msg, Data), log@debug:do_log_event(debug, Tag, Msg, Data)).
+-define(DFLT_LG_ARGS, [{module, ?MODULE}, {function, ?FUNCTION_NAME}, 
+                       {app, ?APP}]).
 
--define(info(Msg), log@debug:do_log_event(info, Msg)).
--define(info(Tag, MsgOrDta), log@debug:do_log_event(info, Tag, MsgOrDta)).
--define(info(Tag, Msg, Data), log@debug:do_log_event(info, Tag, Msg, Data)).
+-define(debug(Msg), 
+            log@debug:do_log_event(debug, Msg, ?DFLT_LG_ARGS)).
+-define(debug(Tag, MsgOrDta), 
+            log@debug:do_log_event(debug, Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(debug(Tag, Msg, Data), 
+            log@debug:do_log_event(debug, Tag, Msg, Data, ?DFLT_LG_ARGS)).
 
--define(notice(Msg), log@debug:do_log_event(notice, Msg)).
--define(notice(Tag, MsgOrDta), log@debug:do_log_event(notice, Tag, MsgOrDta)).
--define(notice(Tag, Msg, Data), log@debug:do_log_event(notice, Tag, Msg, Data)).
+-define(info(Msg), 
+            log@debug:do_log_event(info, Msg, ?DFLT_LG_ARGS)).
+-define(info(Tag, MsgOrDta), 
+            log@debug:do_log_event(info, Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(info(Tag, Msg, Data), 
+            log@debug:do_log_event(info, Tag, Msg, Data, ?DFLT_LG_ARGS)).
 
--define(warning(Msg), log@debug:do_log_event(warning, Msg)).
--define(warning(Tag, MsgOrDta), log@debug:do_log_event(warning Tag, MsgOrDta)).
--define(warning(Tag, Msg, Data), log@debug:do_log_event(warning, Tag, Msg, Data)).
+-define(notice(Msg), 
+            log@debug:do_log_event(notice, Msg, ?DFLT_LG_ARGS)).
+-define(notice(Tag, MsgOrDta), 
+            log@debug:do_log_event(notice, Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(notice(Tag, Msg, Data), 
+            log@debug:do_log_event(notice, Tag, Msg, Data, ?DFLT_LG_ARGS)).
 
--define(error(Msg), log@debug:do_log_event(error, Msg)).
--define(error(Tag, MsgOrDta), log@debug:do_log_event(error, Tag, MsgOrDta)).
--define(error(Tag, Msg, Data), log@debug:do_log_event(error, Tag, Msg, Data)).
+-define(warning(Msg), 
+            log@debug:do_log_event(warning, Msg, ?DFLT_LG_ARGS)).
+-define(warning(Tag, MsgOrDta), 
+            log@debug:do_log_event(warning Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(warning(Tag, Msg, Data), 
+            log@debug:do_log_event(warning, Tag, Msg, Data, ?DFLT_LG_ARGS)).
 
--define(critical(Msg), log@debug:do_log_event(critical, Msg)).
--define(critical(Tag, MsgOrDta), log@debug:do_log_event(critical, Tag, MsgOrDta)).
--define(critical(Tag, Msg, Data), log@debug:do_log_event(critical, Tag, Msg, Data)).
+-define(error(Msg), 
+            log@debug:do_log_event(error, Msg, ?DFLT_LG_ARGS)).
+-define(error(Tag, MsgOrDta), 
+            log@debug:do_log_event(error, Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(error(Tag, Msg, Data), 
+            log@debug:do_log_event(error, Tag, Msg, Data, ?DFLT_LG_ARGS)).
 
--define(alert(Msg), log@debug:do_log_event(alert, Msg)).
--define(alert(Tag, MsgOrDta), log@debug:do_log_event(alert, Tag, MsgOrDta)).
--define(alert(Tag, Msg, Data), log@debug:do_log_event(alert, Tag, Msg, Data)).
+-define(critical(Msg), 
+            log@debug:do_log_event(critical, Msg, ?DFLT_LG_ARGS)).
+-define(critical(Tag, MsgOrDta), 
+            log@debug:do_log_event(critical, Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(critical(Tag, Msg, Data), 
+            log@debug:do_log_event(critical, Tag, Msg, Data, ?DFLT_LG_ARGS)).
 
--define(emergency(Msg), log@debug:do_log_event(emergency,Msg)).
--define(emergency(Tag, MsgOrDta), log@debug:do_log_event(emergency, Tag, MsgOrDta)).
--define(emergency(Tag, Msg, Data), log@debug:do_log_event(emergency,Tag, Msg, Data)).
+-define(alert(Msg), 
+            log@debug:do_log_event(alert, Msg, ?DFLT_LG_ARGS)).
+-define(alert(Tag, MsgOrDta), 
+            log@debug:do_log_event(alert, Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(alert(Tag, Msg, Data), 
+            log@debug:do_log_event(alert, Tag, Msg, Data, ?DFLT_LG_ARGS)).
+
+-define(emergency(Msg), 
+            log@debug:do_log_event(emergency,Msg, ?DFLT_LG_ARGS)).
+-define(emergency(Tag, MsgOrDta), 
+            log@debug:do_log_event(emergency, Tag, MsgOrDta, ?DFLT_LG_ARGS)).
+-define(emergency(Tag, Msg, Data), 
+            log@debug:do_log_event(emergency,Tag, Msg, Data, ?DFLT_LG_ARGS)).
--- src/log@debug.erl
+++ src/log@debug.erl
@@ -1,47 +1,26 @@
 -module(log@debug).
 
--export([do_log_event/2, do_log_event/3, do_log_event/4]).
+-export([do_log_event/3, do_log_event/4, do_log_event/5]).
 
-do_log_event(What, InFmt) ->
-    [{registered_name, Nme}, {current_stacktrace, [_|Stck]}] = 
-        process_info(self(), [registered_name, current_stacktrace]),
-    Nme1 = if Nme =:= [] -> undefined; true -> Nme end,
-    do_log_event1(
-        What, InFmt, [{registered_name, Nme1}, {stacktrace, Stck}]).
+do_log_event(What, {_Fmt, Args}=InFmt, AddArgs) when is_list(Args) ->
+    do_log_event(What, undefined, InFmt, AddArgs);
+do_log_event(What, {tag, Tag}, AddArgs) ->
+    do_log_event(What, Tag, undefined, AddArgs);
+do_log_event(What, {data, _}=Dta, AddArgs) ->
+    do_log_event(What, undefined, Dta, AddArgs);
+do_log_event(What, Msg, AddArgs) ->
+    do_log_event(What, undefined, Msg, AddArgs).
 
-do_log_event(What, Tag, MsgOrDta) ->
-    [{registered_name, Nme}, {current_stacktrace, [_|Stck]}] = 
-        process_info(self(), [registered_name, current_stacktrace]),
-    Nme1 = if Nme =:= [] -> undefined; true -> Nme end,
-    do_log_event1(
-        What, Tag, MsgOrDta, [{registered_name, Nme1}, {stacktrace, Stck}]).
+do_log_event(What, Tag, {_Fmt, Args}=InFmt, AddArgs) when is_list(Args) ->
+    do_log_event(What, Tag, InFmt, undefined, AddArgs);
+do_log_event(What, Tag, {data, Data}, AddArgs) ->
+    do_log_event(What, Tag, undefined, Data, AddArgs);
+do_log_event(What, Tag, Msg, AddArgs) ->
+    do_log_event(What, Tag, Msg, undefined, AddArgs).
 
-do_log_event(What, Tag, Msg, Data) ->
-    [{registered_name, Nme}, {current_stacktrace, [_|Stck]}] = 
-        process_info(self(), [registered_name, current_stacktrace]),
-    Nme1 = if Nme =:= [] -> undefined; true -> Nme end,
-    do_log_event1(
-        What, Tag, Msg, Data,  [{registered_name, Nme1}, {stacktrace, Stck}]).
-
-do_log_event1(What, {_Fmt, Args}=InFmt, AddArgs) when is_list(Args) ->
-    do_log_event1(What, undefined, InFmt, AddArgs);
-do_log_event1(What, {tag, Tag}, AddArgs) ->
-    do_log_event1(What, Tag, undefined, AddArgs);
-do_log_event1(What, {data, _}=Dta, AddArgs) ->
-    do_log_event1(What, undefined, Dta, AddArgs);
-do_log_event1(What, Msg, AddArgs) ->
-    do_log_event1(What, undefined, Msg, AddArgs).
-
-do_log_event1(What, Tag, {_Fmt, Args}=InFmt, AddArgs) when is_list(Args) ->
-    do_log_event1(What, Tag, InFmt, undefined, AddArgs);
-do_log_event1(What, Tag, {data, Data}, AddArgs) ->
-    do_log_event1(What, Tag, undefined, Data, AddArgs);
-do_log_event1(What, Tag, Msg, AddArgs) ->
-    do_log_event1(What, Tag, Msg, undefined, AddArgs).
-
-do_log_event1(What, Tag, {Format, Args}, Data, AddArgs) when is_list(Args) ->
-    do_log_event1(What, Tag, io_lib:format(Format, Args), Data, AddArgs);
-do_log_event1(What, Tag, Msg, Data, AddArgs) ->
+do_log_event(What, Tag, {Format, Args}, Data, AddArgs) when is_list(Args) ->
+    do_log_event(What, Tag, io_lib:format(Format, Args), Data, AddArgs);
+do_log_event(What, Tag, Msg, Data, AddArgs) ->
     logger:What(
         [{tag, Tag}, 
          {desc, Msg},
